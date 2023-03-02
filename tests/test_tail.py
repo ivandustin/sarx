4532dd0d7b43dcbb5151ff9e5e5d4328a67635d9
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-02 19:16:42 +0800

    Add tail

---
+++ tests/test_tail.py
@@ -0,0 +1,12 @@
+from jax.numpy import arange, array, array_equal
+from sarx.tail import tail
+
+
+def test():
+    matrix = arange(1, 10).reshape(3, 3)
+    expected = array([
+        [2, 3],
+        [5, 6],
+        [8, 9]
+    ])
+    assert array_equal(tail(matrix), expected)
