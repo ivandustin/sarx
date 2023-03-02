e0728a7d06daefaae963e7933c9e3fc37f5ece9d
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-02 19:08:38 +0800

    Rename variables

--- sarx/neurogenesis.py
+++ sarx/neurogenesis.py
@@ -5,7 +5,7 @@ from .insert import insert
 
 def neurogenesis(key, net):
     keys = split(key, len(net))
-    a = [insert(synapse(key, shape=(syn.shape[0], 1)), syn)
-         for key, syn in zip(keys, net)]
-    b = [synapse(key, shape=(1, 1))]
-    return a + b
+    old = [insert(synapse(key, shape=(syn.shape[0], 1)), syn)
+           for key, syn in zip(keys, net)]
+    new = [synapse(key, shape=(1, 1))]
+    return old + new
