f571d34938d9b3152e99a1eb483ce25407f7cd68
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-02 18:38:05 +0800

    Add left

---
+++ tests/test_left.py
@@ -0,0 +1,16 @@
+from jax.numpy import array, array_equal
+from sarx.left import left
+
+
+def test():
+    matrix = array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+    expected = array([
+        [1, 2],
+        [4, 5],
+        [7, 8]
+    ])
+    assert array_equal(left(matrix), expected)
