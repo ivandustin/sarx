97e70fa2533ecbc9d7633cc1a298057c724ee1e9
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-02 18:39:11 +0800

    Add right

---
+++ tests/test_right.py
@@ -0,0 +1,16 @@
+from jax.numpy import array, array_equal
+from sarx.right import right
+
+
+def test():
+    matrix = array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+    expected = array([
+        [3],
+        [6],
+        [9]
+    ])
+    assert array_equal(right(matrix), expected)
