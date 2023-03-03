b28e5f29bec247a7ac61d6a44f08400139e22170
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-04 01:06:22 +0800

    Ensure synapse is float32 and strongly typed

--- tests/test_synapse.py
+++ tests/test_synapse.py
@@ -1,10 +1,12 @@
+from jax.numpy import float32, isclose
 from jax.random import PRNGKey
-from jax.numpy import isclose
 from sarx.synapse import synapse
 
 
 def test():
     key = PRNGKey(0)
     weights = synapse(key, shape=(1000,))
+    assert weights.dtype == float32
+    assert not weights.weak_type
     assert isclose(weights.mean(), 1.5, atol=0.01)
     assert isclose(weights.std(), 0.1, atol=0.01)
