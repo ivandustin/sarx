bc337c37dd2bb6d128aa4ef03be8b94a0b9e168a
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-03 20:07:03 +0800

    Test feed

---
+++ tests/test_feed.py
@@ -0,0 +1,34 @@
+from jax.numpy import array, isclose
+from jax.random import PRNGKey
+from jax.lax import fori_loop
+from sarx import network, feed, infer, spike, mae, gd
+
+
+def test():
+    key = PRNGKey(0)
+    net = network(key, 1)
+    x = array([[1.0]])
+    y = array([[1.7]])
+    before = predict(net, x)
+    net = train(net, x, y)
+    after = predict(net, x)
+    assert not isclose(before, y, atol=0.05)
+    assert isclose(after, y, atol=0.05)
+
+
+def train(network, x, y):
+    def body(_, network):
+        return feed(loss, update)(network, x, y)
+    return fori_loop(0, 2, body, network)
+
+
+def predict(network, x):
+    return infer(spike)(network, x)[-1]
+
+
+def loss(network, x, y):
+    return mae(y, predict(network, x))
+
+
+def update(synapse, gradient):
+    return gd(synapse, 0.1, gradient)
