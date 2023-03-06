1248b158a5581c02d21774e4d5b840c803723a44
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-06 23:26:25 +0800

    Remove feed

--- sarx/__init__.py
+++ sarx/__init__.py
@@ -3,7 +3,6 @@ from .forward import forward
 from .network import network
 from .prune import prune
 from .spike import spike
-from .feed import feed
 from .mae import mae
 from .mse import mse
 from .gd import gd
@@ -14,7 +13,6 @@ __all__ = [
     'network',
     'prune',
     'spike',
-    'feed',
     'mae',
     'mse',
     'gd'
--- sarx/feed.py
+++
@@ -1,8 +0,0 @@
-from jax import grad
-
-
-def feed(loss, update):
-    def function(network, x, y):
-        gradient = grad(loss)(network, x, y)
-        return update(network, gradient)
-    return function
