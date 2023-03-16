a7c3d65c678d3cf2c87e9cf5e982c8f6c1f2a4d9
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-03-16 23:46:50 +0800

    Update xor test

--- tests/test_xor.py
+++ tests/test_xor.py
@@ -1,28 +1,49 @@
-from jax.numpy import array, array_equal
+from jax.numpy import array, array_equal, clip
 from jax.random import PRNGKey, split
+from jax.tree_util import tree_map
 from jax.lax import fori_loop
 from jax import grad
 from optax import apply_updates, sgd
 from sarx import apply, loss, network, neurogenesis
+import pytest
 
 
-def test():
+@pytest.mark.parametrize("n", [1, 20])
+@pytest.mark.parametrize("x,y", [
+    (
+        array([
+            [0.0, 0.0],
+            [0.0, 1.0],
+            [1.0, 0.0],
+            [1.0, 1.0]
+        ]),
+        array([
+            [0.0],
+            [1.0],
+            [1.0],
+            [0.0]
+        ])
+    ),
+    (
+        array([
+            [0.0, 0.0],
+            [0.0, 2.0],
+            [2.0, 0.0],
+            [2.0, 2.0]
+        ]),
+        array([
+            [0.0],
+            [2.0],
+            [2.0],
+            [0.0]
+        ])
+    )
+])
+def test(n, x, y):
     key = PRNGKey(0)
-    key_a, key_b = split(key)
-    model = network(key_a, 2)
-    model = neurogenesis(key_b, model)
-    x = array([
-        [0.0, 0.0],
-        [0.0, 2.0],
-        [2.0, 0.0],
-        [2.0, 2.0]
-    ])
-    y = array([
-        [0.0],
-        [2.0],
-        [2.0],
-        [0.0]
-    ])
+    model = network(key, 2)
+    for key in split(key, n):
+        model = neurogenesis(key, model)
     model = train(model, x, y)
     assert array_equal(apply(model, x).clip(0, 1), y.clip(0, 1))
 
@@ -35,6 +56,8 @@ def train(network, x, y):
         network, state = args
         gradient = grad(loss)(network, x, y)
         updates, state = optimizer.update(gradient, state)
-        return apply_updates(network, updates), state
+        network = apply_updates(network, updates)
+        network = tree_map(lambda x: clip(x, -2.0, 2.0), network)
+        return network, state
 
     return fori_loop(0, 30, body, (network, state))[0]
